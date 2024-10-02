da3b9c7f57d67649236ed9d882642743c071223f
Slendi <slendi@socopon.com>
2024-10-02 12:43:32 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
 
       # System types to support.
       supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-apple-darwin" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
